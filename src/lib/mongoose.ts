c5e12b0eb1946d2b7d510e95811ae91e23f21e6b
taytruong <taytruong05@gmail.com>
2026-05-04 16:08:45 +0700

    fix file mongoose.ts

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,14 +1,16 @@
+"use server";
+
 import mongoose from "mongoose";
 
 // singelton connection :  check đã connect chưa ? nếu có MONGODB_URL is already connected
 let isConnected: boolean = false;
 export const connectToDatabase = async () => {
-  if (!process.env.MONGODB_URL) {
-    throw new Error("MONGODB_URL is not set");
-  }
   if (isConnected) {
     console.log("MONGODB_URL is already connected");
     return;
+  }
+  if (!process.env.MONGODB_URL) {
+    throw new Error("MONGODB_URL is not set");
   }
 
   // nếu chưa connect thì chạy try/catch
