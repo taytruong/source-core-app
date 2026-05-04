549eecde22ce87d80e116caf69386d634c327056
taytruong <taytruong05@gmail.com>
2026-05-04 16:16:01 +0700

    remove all file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
-    >
+    <ClerkProvider>
       <html lang="en">
         <body className={`${work__sans.className}`}>{children}</body>
       </html>
