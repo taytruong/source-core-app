e31abc08d460ebded85246567e1dd35d874e63ea
taytruong <taytruong05@gmail.com>
2026-09-17 12:37:24 +0700

    update reponsive banneRight

--- src/shared/components/layout/banner.tsx
+++ src/shared/components/layout/banner.tsx
@@ -26,7 +26,7 @@ function Banner(_props: BannerProps) {
         </div>
       </div>
 
-      <div className="hidden shrink-0 2xl:block 2xl:w-[22%] [&>svg]:h-auto [&>svg]:w-full">
+      <div className="hidden shrink-0 2xl:block 2xl:w-[22%] [&>svg]:h-auto">
         <BannerRight />
       </div>
     </section>
