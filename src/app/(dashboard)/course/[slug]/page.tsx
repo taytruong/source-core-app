ca18500544c6c8b366f60014e139aef5525c63fb
taytruong <taytruong05@gmail.com>
2026-09-18 16:31:16 +0700

    update icon for details page

--- src/app/(dashboard)/course/[slug]/page.tsx
+++ src/app/(dashboard)/course/[slug]/page.tsx
@@ -1,4 +1,8 @@
+import Link from 'next/link';
+
 import { CourseDetailsPage } from '@/src/modules/course/pages';
+import { Heading } from '@/src/shared/components/common';
+import { IconArrowLeft } from '@/src/shared/components/icons';
 import { Header } from '@/src/shared/components/layout';
 
 export interface CourseDetailPageRootProps {
@@ -9,7 +13,20 @@ export interface CourseDetailPageRootProps {
 function CourseDetailPageRoot({ params }: CourseDetailPageRootProps) {
   return (
     <>
-      <Header title="Course Details." />
+      <div className="mb-7 flex h-20 justify-between">
+        <div className="flex items-center gap-3">
+          <Link
+            className="flex items-center rounded-md border border-gray-400 p-1"
+            href={'/'}
+          >
+            <IconArrowLeft />
+          </Link>
+          <Heading className="font-semibold lg:text-3xl">
+            Course Details.
+          </Heading>
+        </div>
+        <Header />
+      </div>
       <CourseDetailsPage slug={params.slug} />
     </>
   );
